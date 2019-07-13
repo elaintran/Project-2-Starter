dc14f0363085d13930316124b12a7dd67ae9bfb9
elaintran <elain.ttran@gmail.com>
2019-07-12 23:47:23 -0500

    created character object

---
+++ public/js/character-select.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    var characters = [
+        {
+            name: "Character 1",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/fighter-sword-red.png",
+            chibi: "images/resource-images/chibi/fighter-sword-red.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 2",
+            class: "Lance Fighter",
+            portrait: "images/resource-images/portrait/fighter-lance-blue.png",
+            chibi: "images/resource-images/chibi/fighter-lance-blue.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 3",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/fighter-axe-green.png",
+            chibi: "images/resource-images/chibi/fighter-axe-green.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 4",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/fighter-bow-purple.png",
+            chibi: "images/resource-images/chibi/fighter-bow-purple.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 5",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/mage-red.png",
+            chibi: "images/resource-images/chibi/mage-red.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 6",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/mage-blue.png",
+            chibi: "images/resource-images/chibi/mage-blue.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 7",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/knight-axe-green.png",
+            chibi: "images/resource-images/chibi/knight-axe-green.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }, {
+            name: "Character 8",
+            class: "Swordmaster",
+            portrait: "images/resource-images/portrait/thief-purple.png",
+            chibi: "images/resource-images/chibi/thief-purple.png",
+            stats: {
+                hp: 10,
+                strength: 10,
+                magic: 10,
+                defense: 10,
+                speed: 10
+            }
+        }
+    ]
+})
