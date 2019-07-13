42fe52f3a2b1bc56d1c41d39250fd8ddab71cddd
elaintran <elain.ttran@gmail.com>
2019-07-12 23:57:42 -0500

    loop through character obj to display chibis

--- public/js/character-select.js
+++ public/js/character-select.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 3",
-            class: "Swordmaster",
+            class: "Axe Fighter",
             portrait: "images/resource-images/portrait/fighter-axe-green.png",
             chibi: "images/resource-images/chibi/fighter-axe-green.png",
             stats: {
@@ -38,7 +38,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 4",
-            class: "Swordmaster",
+            class: "Bow Fighter",
             portrait: "images/resource-images/portrait/fighter-bow-purple.png",
             chibi: "images/resource-images/chibi/fighter-bow-purple.png",
             stats: {
@@ -50,7 +50,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 5",
-            class: "Swordmaster",
+            class: "Red Mage",
             portrait: "images/resource-images/portrait/mage-red.png",
             chibi: "images/resource-images/chibi/mage-red.png",
             stats: {
@@ -62,7 +62,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 6",
-            class: "Swordmaster",
+            class: "Blue Mage",
             portrait: "images/resource-images/portrait/mage-blue.png",
             chibi: "images/resource-images/chibi/mage-blue.png",
             stats: {
@@ -74,7 +74,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 7",
-            class: "Swordmaster",
+            class: "Knight",
             portrait: "images/resource-images/portrait/knight-axe-green.png",
             chibi: "images/resource-images/chibi/knight-axe-green.png",
             stats: {
@@ -86,7 +86,7 @@ $(document).ready(function() {
             }
         }, {
             name: "Character 8",
-            class: "Swordmaster",
+            class: "Thief",
             portrait: "images/resource-images/portrait/thief-purple.png",
             chibi: "images/resource-images/chibi/thief-purple.png",
             stats: {
@@ -98,4 +98,23 @@ $(document).ready(function() {
             }
         }
     ]
+
+    function characterDisplay() {
+        for (var i = 0; i < characters.length; i++) {
+            var chibiContainer = $("<div>").addClass("character-container");
+            var characterImage = $("<img>").attr({
+                "src": characters[i].chibi,
+                "data-class": characters[i].class
+            })
+            chibiContainer.append(characterImage);
+            $(".character-list").append(chibiContainer);
+        }
+    }
+    characterDisplay();
+
+    // $(".character-container").on("click", function() {
+    //     for (var i = 0; i < characters.length; i++) {
+    //         if ($(this))
+    //     }
+    // })
 })
