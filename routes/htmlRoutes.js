21a5f291fff61031cecbbe344ce452474794647f
David Garza <dgarza0413@gmail.com>
2019-07-13 11:15:42 -0500

    paths to character selection linked

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,20 @@
 // var db = require("../models");
 
+var path = require("path");
+
+
 module.exports = function (app) {
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/character", function (req, res) {
+    // res.send("this is the character selection");
+    res.sendFile(path.join(__dirname, "../public/character-select.html"));
+  });
+
+  app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/login"));
   });
 
 
