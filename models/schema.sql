9b1788ea9553bdf4bb7de7ef26134c2142eda7cc
David Garza <dgarza0413@gmail.com>
2019-07-12 20:26:10 -0500

    seeds layout

--- models/schema.sql
+++ models/schema.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS fire_emblem_db;
 CREATE DATABASE fire_emblem_db;
-
 USE fire_emblem_db;
 
 CREATE TABLE users (
@@ -12,27 +11,34 @@ CREATE TABLE users (
 
 CREATE TABLE character_main (
     id INT AUTO_INCREMENT NOT NULL,
-    character_name VARCHAR(100) NOT NULL,
-    character_class VARCHAR(100) NOT NULL,
-    character_hp INT(11) NOT NULL,
-    character_str INT(11) NOT NULL,
-    character_def INT(11) NOT NULL,
-    character_spd INT(11) NOT NULL
+    main_name VARCHAR(100) NOT NULL,
+    main_class VARCHAR(100) NOT NULL,
+    main_hp INT(11) NOT NULL,
+    main_str INT(11) NOT NULL,
+    main_mag INT(11) NOT NULL,
+    main_def INT(11) NOT NULL,
+    main_spd INT(11) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE character_enemy (
-        id INT AUTO_INCREMENT NOT NULL,
-    character_name VARCHAR(100) NOT NULL,
-    character_class VARCHAR(100) NOT NULL,
-    character_hp INT(11) NOT NULL,
-    character_str INT(11) NOT NULL,
-    character_def INT(11) NOT NULL,
-    character_spd INT(11) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL,
+    enemy_name VARCHAR(100) NOT NULL,
+    enemy_class VARCHAR(100) NOT NULL,
+    enemy_hp INT(11) NOT NULL,
+    enemy_str INT(11) NOT NULL,
+    enemy_mag INT(11) NOT NULL,
+    enemy_def INT(11) NOT NULL,
+    enemy_spd INT(11) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE inventory ()
+CREATE TABLE inventory (
+    id INT AUTO_INCREMENT NOT NULL,
+    item_name VARCHAR(100) NOT NULL,
+    item_class VARCHAR(100) NOT NULL,
+    item_potency TINYINT NOT NULL
+);
 
 
 
---
+++ models/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO character_main (main_name , main_class, main_hp, main_str, main_def, main_spd,)
+VALUES  ("knight_name", "knight", 9, 7, 4, 8, 5), 
+        ("fight_name", "fighter", 8, 9, 2, 6, 6),
+        ("mage_name", "mage", 6, 3, 9, 5, 7),
+        ("rouge_name", "rouge", 7, 8, 3, 6, 8)
+
+INSERT INTO character_enemy (enemy_name , enemy_class, enemy_hp, enemy_str, enemy_def, enemy_spd,)
+VALUES  ("knight_name", "knight", 9, 7, 4, 8, 5), 
+        ("fight_name", "fighter", 8, 9, 2, 6, 6),
+        ("mage_name", "mage", 6, 3, 9, 5, 7),
+        ("rouge_name", "rouge", 7, 8, 3, 6, 8)
