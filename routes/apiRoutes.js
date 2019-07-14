85b36d5402285927b6e2d4403530f841eedf9969
David Garza <dgarza0413@gmail.com>
2019-07-13 21:54:01 -0500

    merge complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,17 @@ module.exports = function (app) {
             res.json(dbUsers);
         });
     });
+
+    app.get("/api/users/:id", function (req, res) {
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbUsers) {
+            res.json(dbUsers);
+        });
+    });
+
     app.post("/api/users", function (req, res) {
         db.User.create(req.body).then(function (dbUsers) {
             res.json(dbUsers);
