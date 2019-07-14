27dedbe7218e27dbd243f89488b822ab5bb90200
Christina Truong <christinaqtruong@gmail.com>
2019-07-14 00:42:48 -0500

    added enemy attack

--- js/player.js
+++ js/player.js
@@ -4,7 +4,8 @@ let grabbedTarget;
 let grabbedClass;
 let chance = Math.floor();
 let baseDmg;
-let enemyDmg;
+let enemySpd;
+let enemyBaseDmg;
 
 function Character(classType, hp, def, str, spd){
     this.classType = classType;
@@ -46,10 +47,40 @@ let charMoves = {
         }
 
         let enemyAtk = function(){
-            //randomly select target
-            if(target.target === randomTar){
-                if(chance <= )
+            let randomTar;
+
+            //assigns random target for enemy (1/3 chance)
+            let enemyTar = function(){
+                if(chance <= 0.3){
+                    randomTar = "heads";
+                } else if (0.31 < chance <= 0.6){
+                    randomTar = "body";
+                } else {
+                    randomTar = "legs";
+                }
             }
+            //passes random target to functions
+            if(target.target === randomTar){
+                //if the hit chance is whatever that target's hit chance was (0.25 for head, 0.95 for body), add that target's bonus damage to the attack damage
+                if(chance <= target.hitChance){
+                    enemyBaseDmg = enemy.str + target.bonus;
+                    enemySpd -= target.reduceSpd;
+                } else {
+                        //attack missed
+                        enemyBaseDmg = 0;
+                }
+            }
+
+            return enemyBaseDmg;
+        }
+
+        //if character speed > enemy speed, user attacks first
+        if(charSpd >= enemySpd){
+            charAtk();
+            enemy.hp = enemy.hp - (baseDmg - enemy.def);
+            
+            //reflect damage taken
+            $('#hp-display').innerHTML("Whatever you want here")
         }
     }
 
