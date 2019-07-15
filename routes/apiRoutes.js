65b1a12a7146f624cf10dcf0de034d235ce4ca6c
David Garza <dgarza0413@gmail.com>
2019-07-14 19:08:55 -0500

    routes for auth

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,21 +2,53 @@ var db = require("../models");
 
 module.exports = function (app) {
     // Get all users
-    app.get("/api/users", function (req, res) {
-        db.User.findAll({}).then(function (dbUsers) {
-            res.json(dbUsers);
-        });
-    });
+    app.get("/api/users", (req, res) =>
+        db.User.findAll({}).then((dbUser) =>
+            res.json(dbUser)
+        ));
 
-    app.get("/api/users/:id", function (req, res) {
+    app.get("/api/users/:id", (req, res) =>
+        db.User.findOne({
+            where: { id: req.params.id }
+        }).then((dbUsers) => res.json(dbUsers))
+    )
+
+    app.post("/api/login", (req, res) =>
         db.User.findOne({
             where: {
-                id: req.params.id
+                userEmail: req.body.userEmail
             }
-        }).then(function (dbUsers) {
-            res.json(dbUsers);
-        });
-    });
+        }).then((dbUser) =>
+            (dbUser.userPassword === req.body.userPassword) ?
+                res.json(dbUser) : res.send("nope")
+        )
+    );
+
+    app.post("/api/register", (res, req) => {
+        db.User.create({
+            userEmail: req.body.userEmail,
+            userPassword: req.body.userPassword
+        }).then((dbUsers) => res.json(dbUsers))
+    })
+    // app.post("/api/users", (req, res) =>
+    //     db.User.create(req.body).then((dbUser => res.json(dbUser)))
+    // )
+
+    // app.get("/api/users", function (req, res) {
+    //     db.User.findAll({}).then(function (dbUsers) {
+    //         res.json(dbUsers);
+    //     });
+    // });
+
+    // app.get("/api/users/:id", function (req, res) {
+    //     db.User.findOne({
+    //         where: {
+    //             id: req.params.id
+    //         }
+    //     }).then(function (dbUsers) {
+    //         res.json(dbUsers);
+    //     });
+    // });
 
     app.post("/api/users", function (req, res) {
         db.User.create(req.body).then(function (dbUsers) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,28 +4,77 @@ var path = require("path");
 
 
 module.exports = function (app) {
+  app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/landing.html"))
+  );
+
+  app.get("/login", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/login.html"))
+  );
+
+  app.get("/register", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/register.html"))
+  );
+
+  app.post("/register", (req, res) => {
+    const { userName, userEmail, userPassword, userPassword2 } = req.body;
+    let errors = [];
+    // check required field
+    if (!userName || !userEmail || !userPassword) {
+      errors.push({ msg: 'Please fill in all fields' });
+    }
+    // check passwrods match
+    if (userPassWord !== userPassword2) {
+      errors.push({ msg: "password/email is incorrect" })
+    }
+    // check pass length
+    if (userPassword.length < 6) {
+      errors.push({ msg: "Password must be six characters or greater" })
+    }
+
+    if (errors.length > 0) {
+      res.render("register", {
+        errors,
+        userName,
+        userEmail,
+        userPassWord,
+        userPassword2
+      })
+    } else {
+      res.send("pass")
+    }
+  })
+
+  app.get("/character", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/character-select.html"))
+  );
+
+  //post data about character selection
+  app.post("")
+
+  app.get("/encounter", (req, res) =>
+    res.sendFile(path.join(__dirname, "../public/encounter.html"))
+  );
+
+  app.get("*", (req, res) =>
+    res.render("404")
+  );
+
   // app.get("/", function (req, res) {
   //   res.sendFile(path.join(__dirname, "../public/login.html"));
   // });
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  })
+  // app.get("/character", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/character-select.html"));
+  // });
 
-  app.get("/register", (req, res) =>
-    res.sendFile(path.join(__dirname, "../public/register.html"));
+  // app.get("/encounter", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/character-encounter.html"));
+  // });
 
-  app.get("/character", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/character-select.html"));
-  });
-
-  app.get("/encounter", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/character-encounter.html"));
-  });
-
-  app.get("*", function (req, res) {
-    res.render("404");
-  });
+  // app.get("*", function (req, res) {
+  //   res.render("404");
+  // });
 
   // Load index page
   // app.get("/", function (req, res) {
