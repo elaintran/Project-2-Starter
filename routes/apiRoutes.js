bf89e134d0307a45f609be6ef487553a030b15cd
David Garza <dgarza0413@gmail.com>
2019-07-15 14:24:17 -0500

    pull

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,27 +13,27 @@ module.exports = function (app) {
         }).then((dbUsers) => res.json(dbUsers))
     );
 
-    app.get("/api/login", (req, res) =>
-        db.User.findOne({
-            where: {
-                userEmail: req.body.userEmail
-            }
-        }).then((dbUsers) =>
-            (dbUsers.userPassword === req.body.userPassword) ?
-                res.json(dbUsers) : res.send("not found")
-        )
-    );
+    // app.get("/api/login", (req, res) =>
+    //     db.User.findOne({
+    //         where: {
+    //             userEmail: req.body.userEmail
+    //         }
+    //     }).then((dbUsers) =>
+    //         (dbUsers.userPassword === req.body.userPassword) ?
+    //             res.json(dbUsers) : res.send("not found")
+    //     )
+    // );
 
-    app.post("/api/register", (req, res) => {
-        console.log(req.body);
-        // use bcrypt to hash pw before sending to db
-        db.User.create({
-            userName: req.body.userName,
-            userEmail: req.body.userEmail,
-            userPassword: req.body.userPassword
-            // logic if not unique then return a msg - flash
-        }).then(
-            // if(check if unique)
-            (dbUser) => res.json(dbUser));
-    });
+    // app.post("/api/register", (req, res) => {
+    //     console.log(req.body);
+    //     // use bcrypt to hash pw before sending to db
+    //     db.User.create({
+    //         userName: req.body.userName,
+    //         userEmail: req.body.userEmail,
+    //         userPassword: req.body.userPassword
+    //         // logic if not unique then return a msg - flash
+    //     }).then(
+    //         // if(check if unique)
+    //         (dbUser) => res.json(dbUser));
+    // });
 };
