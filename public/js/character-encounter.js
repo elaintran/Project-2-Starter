ea9449fe8364b5fd72ede8dee2a6ffbe6bbb2fc8
mbaker <mbaker.github@gmail.com>
2019-07-16 09:38:55 -0500

    fixed javascript for attack area hover

--- public/js/character-encounter.js
+++ public/js/character-encounter.js
@@ -1,17 +1,16 @@
-var showTooltip = function(part) {
-    $(".attack-" + part).css({
-        'opacity': '1',
-        'left': '0'
+$(".cls-1").each(function() {
+    
+    var target = ($(this).parent().attr("data-part"));
+    // console.log(target);
+    $(this).hover(function() {
+        $(".attack-" + target).css({
+            "opacity": "1",
+            "left": "0"
+        });
+    },function() {
+        $(".attack-" + target).css({
+            "opacity": "0",
+            "left": "-1vw"
+        });
     });
-};
-
-var hideTooltop = function(part) {
-    $(".attack-" + part).css({
-        'opacity': '0',
-        'left': '-1vw'
-    });
-};
-
-$("#knight_head > .cls-1").hover(showTooltip(head), hideTooltop(head));
-$("#knight_body > .cls-1").hover(showTooltip(body), hideTooltop(body));
-$("#knight_legs > .cls-1").hover(showTooltip(legs), hideTooltop(legs));
+});
